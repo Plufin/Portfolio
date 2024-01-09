f8ad62442924c745d6c18a248f22f90e79e22191
Plufin <118924716+Plufin@users.noreply.github.com>
2024-01-09 13:27:35 +0200

    test about design

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,44 +1,157 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Global from '../components/global';
 import styled from 'styled-components';
-import Global from '../components/global.js';
 
-const About = () => {
+function About() {
   return (
-    <Container>
+    <div className="base">
       <Global />
-      <AboutTitle>About Me</AboutTitle>
-      <AboutContent>
-        <p>
-          Hello! My name is [Your Name], and I am a passionate [your profession/hobbies].
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore
-          et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat.
-        </p>
-        <p>
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </AboutContent>
-    </Container>
+      <Header>
+        <Title>
+          <h2>SUMMARY</h2>
+          <h2>OF THIS SECTION</h2>
+        </Title>
+      </Header>
+      <Content>
+        <Box>
+          <Right>
+            <Thumbnail src={require('../components/Resources/hex.jpg')} alt="Hex" />
+            <Text>
+              <h2>TEXT 1</h2>
+              <p>Donec ac felis accumsan, ultrices augue sit amet, vestibulum sapien. Donec in est non dolor dignissim luctus sit amet at turpis. Aliquam lacinia, magna vitae accumsan ultricies, mauris velit fringilla felis, sed cursus nisi nisi sit amet turpis. Nulla convallis orci a tincidunt elementum. Sed posuere mattis laoreet. </p>
+            </Text>
+          </Right>
+          <BottomRight />
+        </Box>
+        <Box>
+          <Left>
+            <Text>
+              <h2>TEXT 2</h2>
+              <p>Ut laoreet est et massa posuere scelerisque. Cras dui velit, interdum a nulla eget, fringilla elementum risus. Nulla est lectus, cursus nec vestibulum a, porta id tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc fermentum at nibh id pharetra. Phasellus vitae tellus dui.</p>
+            </Text>
+            <Thumbnail src={require('../components/Resources/fish.jpg')} alt="Fish" />
+          </Left>
+          <BottomLeft />
+        </Box>
+        <Box>
+          <Right>
+            <Thumbnail src={require('../components/Resources/knot.jpg')} alt="Knot" />
+            <Text>
+              <h2>TEXT 3</h2>
+              <p>Cras dui velit, interdum a nulla eget, fringilla elementum risus. Nulla est lectus, cursus nec vestibulum a, porta id tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc fermentum at nibh id pharetra. Phasellus vitae tellus dui.</p>
+            </Text>
+          </Right>
+          <BottomRight />
+        </Box>
+        <Box>
+          <Left>
+            <Text>
+              <h2>TEXT 4</h2>
+              <p>Nulla est lectus, cursus nec vestibulum a.</p>
+            </Text>
+            <Thumbnail src={require('../components/Resources/cave.jpg')} alt="Cave" />
+          </Left>
+          <BottomLeft />
+        </Box>
+      </Content>
+    </div>
   );
-};
+}
 
-const Container = styled.div`
-  max-width: 800px;
-  margin: 0 auto;
-  padding: 20px;
+const Header = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding-left: 10%;
+  padding-top: 100px;
+  max-height: 250px;
 `;
 
-const AboutTitle = styled.h2`
-  text-align: center;
-  margin-bottom: 20px;
+const Title = styled.div`
+  border-bottom: 1px solid #FCD864;
+  min-width: 150px;
+  max-width: 350px;
 `;
 
-const AboutContent = styled.div`
-  line-height: 1.6;
+const Content = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
 
-  p {
-    margin-bottom: 15px;
+const Box = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow:visible;
+  justify-content: center;
+  `;
+
+const Bottom = styled.div`
+margin-top: 100px;
+margin-bottom:100px;
+height: 70px;
+background-color: #47358C40;
+border: 100px 0px 0px 0px #FCD864; 
+  box-shadow: 0 4px 2px -2px #FCD864;
+`;
+
+const Left = styled.div`
+margin-top: 150px;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  align-items: flex-start;
+  max-height: 300px;
+  overflow: visible;
+`;
+
+const BottomLeft = styled(Bottom)`
+  margin-right: 7%;
+`;
+
+const BottomRight = styled(Bottom)`
+  margin-left: 7%;
+`;
+
+
+
+const Right = styled.div`
+margin-top: 150px;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+max-height: 300px;
+justify-content: center;
+`;
+
+const Thumbnail = styled.img`
+margin-right: 20px;
+padding-top: 40px;
+z-index: 10;
+  width: 100%;
+  max-width: 400px;
+  min-width: 150px;
+`;
+
+const Text = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding-top: 40px;
+  padding-left: 20px;
+  padding-right: 20px;
+  max-width: 700px;
+  min-width: 150px;
+
+  h2 {
+    color: #FCD864;
+    font-size: 30px;
+    font-weight: 400;
+    padding-bottom: 20px;
   }
 `;
 
