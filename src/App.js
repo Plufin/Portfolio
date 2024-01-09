fdabde33011e0dbf3056f4ad6b9687c3c813af17
Plufin <118924716+Plufin@users.noreply.github.com>
2024-01-09 12:29:42 +0200

    testing routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Skills from './pages/Skills';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import Gallery from './pages/Gallery';
+
+import Manatee from './pages/Manatee/Manatee';
+import Narwhal from './pages/Narwhal/Narwhal';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path='*' element={<h1>Page Not Found</h1>} />
-      </Routes>
+      <div className="wrapper">
+        <h1>Marine Mammals</h1>
+        <nav>
+          <ul>
+            <li><Link to="/manatee">Manatee</Link></li>
+            <li><Link to="/narwhal">Narwhal</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/manatee" element={<Manatee />} />
+          <Route path="/narwhal" element={<Narwhal />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
