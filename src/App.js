dcf82d83cf18a90ebe4024c56b44bfe5a2d8d952
Plufin <118924716+Plufin@users.noreply.github.com>
2024-01-09 13:10:32 +0200

    test nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -12,6 +12,28 @@ function App() {
   return (
     <BrowserRouter>
       <div className="wrapper">'
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="about">About</Link>
+            </li>
+            <li>
+              <Link to="skills">Skills</Link>
+            </li>
+            <li>
+              <Link to="projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="gallery">Gallery</Link>
+            </li>
+          </ul>
+        </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
